e9602d2bb2f102ef1ecb0dd998ee7b34553239f8
safe-test <safe.test-1@mail.ru>
2019-03-20 18:04:34 +0300

    simple Multiplication Table

--- src/main/java/MyPackage/MultiplicationTable.java
+++ src/main/java/MyPackage/MultiplicationTable.java
@@ -6,14 +6,9 @@ public class MultiplicationTable {
         for (i = 1; i < 10; i++) {
             for (j =1; j < 10; j++)
             {
-                res = getMultiplicationTable(i, j);
+                res = i * j;
                 System.out.println(i+"*"+j+"="+res);
             }
         }
-    }
-    static int getMultiplicationTable (int i, int j) {
-        int multiplyResult;
-        multiplyResult = i * j;
-        return multiplyResult;
     }
 }
